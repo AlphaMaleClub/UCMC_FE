583eea6c26476a319642596a7dc5bf80a6ebee25
Philo-LEE <tlgus7777@gmail.com>
2025-04-24 01:31:40 +0900

    WIP: 1.AXIOS 튜닝 거의 완료. 익일 테스트하고 체크해볼 것.

--- src/service/AxiosConfig.js
+++ src/service/AxiosConfig.js
@@ -1,23 +1,6 @@
 import axios from "axios";
 const API_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || "http://localhost:3000";
 
-//accessToken을 동적으로 로딩해서 DefaultInstance의 header에 추가한다.
-axiosDefaultInstance.interceptors.request.use((config) => {
-    const token = localStorage.getItem("accessToken");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
-
-//refreshToken 갱신하는 코드
-const refreshAccessToken = async() => {
-    const response = await axios.post("/api/refreshToken", {}, {
-        withCredentials: true,
-    });
-    return response.data.accesstoken
-};
-
 //먼저 요청 보내는 코드임.(LocalStorage 에서 보냄)
 //여기서 날라가는건 AccessToken
 const axiosDefaultInstance = axios.create({
@@ -26,6 +9,75 @@ const axiosDefaultInstance = axios.create({
         "Content-Type": "application/json",
     },
 })
+
+//refreshToken 갱신하는 코드
+const refreshAccessToken = async() => {
+    const response = await axios.post("/api/refreshToken", {}, {
+        withCredentials: true,
+    });
+    return response.data.accessToken
+};
+
+
+// accessToken을 요청할 때 요청을 가로채서(인터셉트) 로딩해서 DefaultInstance의 header에 추가한다.
+axiosDefaultInstance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+
+/* 
+    accessToken을 요청하고 받은 반환값(response)을 가로채서(인터셉트)
+    해당 오류가 accessToken만료로 인한 반환값이면 refreshToken을 통해 다시 accessToken을 발급받고 자동으로 그걸 요청에 넣어서 보냄
+    그리고 그 요청에서 실패한다면 세션만료로 인한 로그아웃 처리하기. 
+*/
+axiosDefaultInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      const originalRequest = error.config;
+  
+      if (error.response?.status === 401 && !originalRequest._retry) { //옵셔널 체이닝으로 확인
+        originalRequest._retry = true;
+        try {
+
+          const newAccessToken = await refreshAccessToken();
+          localStorage.setItem("accessToken", newAccessToken);
+          originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+          return axiosDefaultInstance(originalRequest);
+          
+        } catch (err) {
+          localStorage.removeItem("accessToken");
+          alert("세션이 만료되었습니다. 다시 로그인해주세요.");
+          window.location.href = "/login";
+          throw err;
+        }
+      }
+  
+      return Promise.reject(error);
+    }
+  );
+
+
+
+
+
+// const autoRefreshTokenAndreTry = async (originalRequest) => {
+//     try {
+//       const newAccessToken = await refreshAccessToken();
+//       localStorage.setItem("accessToken", newAccessToken);
+//       originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+//       return axiosDefaultInstance(originalRequest);
+//     } catch (error) {
+//       localStorage.removeItem("accessToken");
+//       alert("세션이 만료되었습니다. 다시 로그인해주세요.");
+//       window.location.href = "/login";
+//       throw error;
+//     }
+//   };
+  
 
 
 
